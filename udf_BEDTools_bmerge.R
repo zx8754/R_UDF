df46c93dacb7e54fec026e6ea038736bb3541de5
Tokhir Dadaev <zx8754@gmail.com>
2014-03-13 14:12:32 +0000

    Update udf_BEDTools_bmerge.R

--- udf_BEDTools_bmerge.R
+++ udf_BEDTools_bmerge.R
@@ -1,8 +1,16 @@
 udf_BEDTools_bmerge <- 
   function(bed1,
-           bmerge,
+           bmerge="/bedtools2-2.19.1/bin/mergeBed",
            opt.string="-s"){
-    #opt.string
+    #bed1 - data.frame sorted by chrom and start.
+    #       If opt.string="-s", then data.frame must be in BED6 format:
+    #             chrom, start, end, name, score, and strand.
+    #       Else: chrom, start, end
+    #
+    #bmerge - BEDTools bmerge file with a path:
+    #     e.g.: /software/bedtools2-2.19.1/bin/mergeBed
+    #
+    #opt.string - 
     #-s   Force strandedness. That is, only merge features that
     #     are the same strand. By default, this is disabled.
     #-n   Report the number of BED entries that were merged.
@@ -13,8 +21,8 @@ udf_BEDTools_bmerge <-
     #-nms Report the names of the merged features separated by semicolons.
     
     #create temp files
-    bedfile=tempfile(tmpdir=getwd())
-    out=tempfile(tmpdir=getwd())
+    bedfile=tempfile(pattern="temp_in_",tmpdir=getwd(),fileext=".txt")
+    out=tempfile(pattern="temp_out_",tmpdir=getwd(),fileext=".txt")
     options(scipen=99) # not to use scientific notation when writing out
     
     #write bed formatted dataframes to tempfile
@@ -23,12 +31,12 @@ udf_BEDTools_bmerge <-
                 col.names=FALSE,row.names=FALSE)
     
     # create the command string and call the command using system()
-    command=paste(bmerge,"-i",opt.string, bedfile,">",out,sep=" ")
+    command=paste(bmerge,"-i",bedfile,opt.string,">",out,sep=" ")
     cat(command,"\n")
     try(system(command))
     
     # read output file and return
     res=read.table(out,header=F)
-    unlink(a.file);unlink(b.file);unlink(out)
+    unlink(bedfile);unlink(out)
     return(res)
   }
