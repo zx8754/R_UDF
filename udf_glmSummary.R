2e75aed4e76f607b15da7622b00aecc184478f18
Tokhir Dadaev <zx8754@gmail.com>
2014-08-15 16:58:17 +0100

    Update udf_glmSummary.R

--- udf_glmSummary.R
+++ udf_glmSummary.R
@@ -4,31 +4,36 @@ udf_glmSummary <- function(model, Description=NA, Summary=FALSE)
   #Get LCI, OR, UCI (lower Confidence, odds ratio, upper confidence)
   
   #get summary
-  coeffs <- coef(summary(model)) 
+  coeffs <- as.data.frame(coef(summary(model)))
   #c("Estimate","Std. Error","z value","Pr(>|z|)")
+  #c("Estimate" ,"Std. Error","t value","Pr(>|t|)")
   
-  #extract
-  Est  <- round(coeffs[ ,"Estimate"],2) #Estimate
-  SE   <- round(coeffs[ ,"Std. Error"],2) #Std Error
-  Z    <- round(coeffs[ ,"z value"],2) #Z Value
-  P    <- format(coeffs[ ,"Pr(>|z|)"],scientific=T) #P Value
-  OR   <- round(exp(Est),2) #Odds Ratio
-  Lo.CI<- round(exp(Est - 1.96 * SE),2) #Lower Confidence Interval
-  Up.CI<- round(exp(Est + 1.96 * SE),2) #Upper Confidence Interval
+  coeffs$OR   <- round(exp(coeffs$Estimate),2) #Odds Ratio
+  coeffs$Lo.CI<- round(exp(coeffs$Estimate - 1.96 * coeffs$"Std. Error"),2) #Lower Confidence Interval
+  coeffs$Up.CI<- round(exp(coeffs$Estimate + 1.96 * coeffs$"Std. Error"),2) #Upper Confidence Interval
+  
+  coefColNames <- colnames(coeffs)
+  colnames(coeffs) <- c("Est","SE",
+                        toupper(substr(coefColNames[grepl("value",coefColNames)],1,1)),
+                        "P","OR","Lo.CI","Up.CI")
+  
+  coeffs[,c(1,2,3)] <- round(coeffs[,c(1,2,3)],2)
+  coeffs$P <- format(coeffs$P,scientific=T)
   
   #output as data.frame
   if(Summary)
   {
     result <- as.data.frame(
-      cbind(Description, P, paste0(OR," (", Lo.CI,"-", Up.CI,")")),
+      cbind(Description, coeffs$P, paste0(coeffs$OR," (", coeffs$Lo.CI,"-", coeffs$Up.CI,")")),
       stringsAsFactors=F)
-    result <- result[!grepl("Intercept",rownames(result)),]
+    #exclude intercept
+    result <- result[-1,]
     colnames(result) <- c("Description","P","OR (CI.95)")
     row.names(result)<-NULL
   } else 
   {
     result <- as.data.frame(
-      cbind(Description, Est, SE, Z, P, OR, Lo.CI, Up.CI),
+      cbind(Description, coeffs),
       stringsAsFactors=F)
   }
   return(result)
